1d129ff75d8ef997cd07f822303c46624e366ae1
KatiLong <katileighlong@gmail.com>
2018-10-23 00:13:49 -0700

    JS reorganized

--- client.js
+++ client.js
@@ -1,6 +1,16 @@
-$(() => {
+//Funciton Declarations
+function imageRatio() {
     var div = $('.page-div');
+    var img = $('#about img')
     var width = div.width();
     
     div.css('height', (width*(2/3)));
+    img.css('height', (width*(2/3)));
+}
+
+//Event Triggers
+window.addEventListener("resize", imageRatio());
+//DOM Doc Ready
+$(() => {
+    imageRatio();
 });
