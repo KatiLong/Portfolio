11e8ede7f736aded8d62b7d44f9d7839f83d0e69
KatiLong <katileighlong@gmail.com>
2018-10-23 11:03:01 -0700

    Name card and mobile nav bar progress

--- client.js
+++ client.js
@@ -1,11 +1,9 @@
 //Funciton Declarations
 function imageRatio() {
-    var div = $('.page-div');
-    var img = $('#about img')
+    var div = $('.page-block');
     var width = div.width();
     
     div.css('height', (width*(2/3)));
-    img.css('height', (width*(2/3)));
 }
 
 //Event Triggers
