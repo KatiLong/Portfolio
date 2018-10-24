4784d384a10b451559ac89e54dc0794cb678ebb4
KatiLong <katileighlong@gmail.com>
2018-10-23 23:40:18 -0700

    Shipping round 2

--- client.js
+++ client.js
@@ -3,15 +3,21 @@ function imageRatio() {
     let div = $('.page-block');
     let project = $('.project-wrapper');
     let width = div.width();
-    let about = $('#about-info');
-    let nameHeight = $('#name-card').height();
+    
 
     //for larger resolutions
     if ($("#about-info").css("display") == "inline-block") { //black/largest media query
-        console.log(nameHeight, about);
         div.css('min-height', (width*(2/3)));
-        about.css('min-height', nameHeight);
         project.css('min-height', width*(.82));
+        let nameHeight = $('#name-card').height();
+        let about = $('#about-info');
+        if (about.height() < nameHeight) {
+            console.log('name card is more');
+            about.css('min-height', nameHeight);
+        } else if (nameHeight < about.height()){ 
+            console.log('about is more');
+            $('#name-card').css('min-height', about.height());
+        };
     } else if ($(".mobile-nav-toggle").css("display") == "none") { //red media query
         div.css('min-height', (width*(.5)));
         project.css('min-height', width*(.66));
@@ -23,7 +29,7 @@ function imageRatio() {
         project.css('min-height', width*(.82));
     }
 }
-
+//Event Triggers
 $('#about-nav').click(() => {
     console.log('about nav ran');
     $('html, body').animate({
@@ -42,8 +48,20 @@ $('#projects-nav').click(() => {
         scrollTop: $('#projects').offset().top - 100
     }, 200);
 });
-//Event Triggers
+// When User clicks on Mobile menu, display or hide nav bar
+$('.mobile-nav-toggle').click(()=> {
+    $('nav').toggle();
+    $('.navbar-header').hide();
+})
+$('nav').click(() => {
+    if ($('.mobile-nav-toggle').css('display') == 'block') {
+        console.log('Mobile nav displayed');
+        $('nav').hide();
+    }
+})
+
 window.addEventListener("resize", imageRatio());
+// window.addEventListener("resize", () => console.log(window.width()));
 //DOM Doc Ready
 $(() => {
     imageRatio();
